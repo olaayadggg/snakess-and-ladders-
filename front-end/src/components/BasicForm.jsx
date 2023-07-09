12d795cad0cf4afae34e5a69492ca84e9a7567f3
Tasbeh mohamed <tasbehmohamed28@gmail.com>
2023-07-09 11:05:36 +0200

    frontend structure

---
+++ front-end/src/components/BasicForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BasicForm() {
+    return (
+        <form autoComplete='off'>
+            <label htmlFor='email'> Email</label>
+        </form>
+    )
+}
