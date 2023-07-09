fe6a0169a6c8f0efbd1a12397d5b0b5e79aef50b
amrrhassan <amrhassan98@hotmail.com>
2023-07-09 14:44:43 +0300

    front-end start

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import img from '../src/istockphoto-455302535-612x612.jpg';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import landingPage from './components/landingPage'
+// import img from '../src/istockphoto-455302535-612x612.jpg';
 
 export default function App() {
   return (
-    <div>
-      <img style={{ width: '100%', height: '100%' }} src={img} alt="" />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        
+        <Route exact path='/' component={landingPage} />
+      </Switch>
+    </BrowserRouter>
+    // <div>
+    //   <img style={{ width: '100%', height: '100%' }} src={img} alt="" />
+    // </div>
   );
 }
