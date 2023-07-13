cdd50e65f0da3e8c096c69f7acbf9d0f5bf165cf
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2023-07-13 14:27:16 +0300

    update comment

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = zReactDOM.createRoot(document.getElementById('root'));
 root.render( <
     React.StrictMode >
     <
     App / >
-    <
-    /React.StrictMode>
+    
+    </React.StrictMode>
 );
+
+// hello world
